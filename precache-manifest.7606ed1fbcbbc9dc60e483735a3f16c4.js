d01987699009f7efe59de8c24e38d03660add503
matan ladani <matanladani@gmail.com>
2020-04-25 18:26:04 +0300

    Updates

---
+++ precache-manifest.7606ed1fbcbbc9dc60e483735a3f16c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6a519b764bfe3531f6b7072c5c477f7",
+    "url": "/monters-rolodex/index.html"
+  },
+  {
+    "revision": "fe75fdb28d405c065d37",
+    "url": "/monters-rolodex/static/css/main.c0718bae.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe75fdb28d405c065d37",
+    "url": "/monters-rolodex/static/js/main.2790986a.chunk.js"
+  },
+  {
+    "revision": "d675114f4bc5dcac6e2b",
+    "url": "/monters-rolodex/static/js/runtime-main.e042b115.js"
+  }
+]);
